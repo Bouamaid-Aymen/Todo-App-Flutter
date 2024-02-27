cb0a69bdade4d213926c1759680d234bb430c726
Aymen Bouamaid <aymenbouamaid919@gmail.com>
2024-02-27 21:37:11 +0100

    Building User Interface & Create Todo Task [POST]

---
+++ lib/screens/add-page.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: submitData,
+            child: Text('Sumbit'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> submitData() async {
+    final Title = titleController.text;
+    final description = descriptionController.text;
+    final Body = {
+      "title": Title,
+      "description": description,
+      "is_completed": true,
+    };
+
+    const url = "https://api.nstack.in/v1/todos";
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(Body), headers: {'Content-Type': 'application/json'});
+    if (response.statusCode == 201) {
+      showSuccessMessage('Creation Success');
+    } else {
+      showErroMessage('Creation failed');
+    }
+  }
+
+  //API Response Recation
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErroMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
